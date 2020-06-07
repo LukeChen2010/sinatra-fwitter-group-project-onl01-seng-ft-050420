cfe9648ae4a403207aa12c655ee0635c7d647d1b
Luke Chen <luke.chen.2010@gmail.com>
2020-06-07 18:30:41 +0000

    Automatically backed up by Learn

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -65,6 +65,7 @@ class TweetsController < ApplicationController
       @tweet = Tweet.find_by(id: id, user_id: current_user.id)
       if @tweet != nil
         @tweet.content = content
+        @tweet.save
       else
         redirect to '/tweets'
       end
